64b371a89dc3bbe12f021c3b9f04c15ff0a815df
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-02-05 15:59:20 +0000

    learning about counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -10,15 +10,32 @@ function App() {
   const addvalue=()=>{
     if(counter<20){
     console.log('clicked',counter);
-    setCounter(counter+1)
+    setCounter(counter+1)//16
+
+    /* Understanding Working of Fibre as batches.
+    For updating counter 4 times expected is 20 but as 
+    fibre works in batches it will do updation only 1 time for repeated
+    statements OUTPUT counter=17 */
+   
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+
+
+    /*Handling this setCounter is a callback fxn*/
+    // setCounter((prevCounter)=>prevCounter+1)
+    // setCounter((prevCounter)=>prevCounter+1)
+    // setCounter((prevCounter)=>prevCounter+1)
+    // setCounter((prevCounter)=>prevCounter+1) OUTPUT-->20
     }
   }
-  const decreasevalue=()=>{
-    if(counter>0){
-    console.log('clicked',counter);
-    setCounter(counter-1)
-    }
-  }
+  // const decreasevalue=()=>{
+  //   if(counter>0){
+  //   console.log('clicked',counter);
+  //   setCounter(counter-1)
+  //   }
+  //}
 
   return (
     <>
@@ -26,8 +43,8 @@ function App() {
      <h2>Counter Value:{counter}</h2>
      <button
      onClick={addvalue}>Increment</button>
-     <button
-     onClick={decreasevalue}>Decrement</button>
+     {/* <button
+     onClick={decreasevalue}>Decrement</button> */}
     </>
   )
 }
