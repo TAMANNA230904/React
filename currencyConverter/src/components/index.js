2d08ae5a6bdab1ca861ece04228e111f810edb3a
Tamanna Sheikh <tamannasheikh2304@gmail.com>
2024-02-09 07:22:06 +0000

    custom hooks

---
+++ currencyConverter/src/components/index.js
@@ -0,0 +1,4 @@
+import InputBox from './InputBox'
+export{InputBox}
+
+//when making large projects this makes things easier
--- currencyConverter/src/hooks/usecurrencyinfo.js
+++ currencyConverter/src/hooks/usecurrencyinfo.js
@@ -1,7 +1,13 @@
 import {useEffect,useState} from "react"
 
 function useCurrrencyInfo(currency){
+    const [data,Setdata]=useState({})
     useEffect(()=>{
-        fetch()
-    },[])
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`).then((res)=>res.json() ).then((res)=>Setdata(res[currency]))
+        console.log(data);
+    },[currency])
+    console.log(data)
+    return data
 }
+
+export default useCurrrencyInfo;
